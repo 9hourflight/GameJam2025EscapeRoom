3ad378aa1f5214b6f8d07848f9ea5e5d7c64819e
DylanDixon-Guilty <dylan.dixon@g.austincc.edu>
2025-11-27 15:48:05 -0600

    Now can obtain key a open door.

--- Assets/Scripts/InteractObjects/Interactable.cs
+++ Assets/Scripts/InteractObjects/Interactable.cs
@@ -3,23 +3,45 @@ using UnityEngine.Events;
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 
 public class Interactable : MonoBehaviour
 {
     Outline outline;
     public string message;
-
     public UnityEvent onInteraction;
+
+    [SerializeField] private GameObject doorPivot;
+
+    private Animator doorOpening;
 
     private void Start()
     {
         outline = GetComponent<Outline>();
+        if(doorPivot != null)
+        {
+            doorOpening = GetComponentInParent<Animator>();
+        }
         DisableOutline();
     }
 
     public void Interact()
     {
         onInteraction.Invoke();
+    }
+
+    public void OnKeyPickedUp()
+    {
+        GameManager.Instance.IsKeyPickedUp = true;
+        Destroy(GameManager.Instance.KeyToDoor);
+    }
+
+    public void OnDoorClicked()
+    {
+        if(GameManager.Instance.IsKeyPickedUp)
+        {
+            doorOpening.SetTrigger("OpenDoor");
+        }
     }
 
     public void DisableOutline()
